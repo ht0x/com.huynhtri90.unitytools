04d1c172ce3683cf7dc5831fdd75118985fa2a40
Huynh Tri <huynhdinh.tri90@gmail.com>
2023-10-31 15:01:11 +0700

    * Updated package installation logic and package json

--- Editor/ToolMenuEditor.cs
+++ Editor/ToolMenuEditor.cs
@@ -1,10 +1,13 @@
+using System.Threading.Tasks;
 using UnityEditor;
 
 namespace HuynhTri
 {
     public static class ToolMenuEditor
     {
-        [MenuItem("Tools/Setup Project/Create Default Folders")]
+        #region ----- Default Project Folders -----
+        
+        [MenuItem("Tools/TriHD/Setup Project/Create Default Folders")]
         static void CreateDefaultFolders()
         {
             if (!DefaultProjectFolders.CreateDirectories())
@@ -20,5 +23,52 @@ namespace HuynhTri
             
             AssetDatabase.Refresh();
         }
+        
+        #endregion
+        
+        #region ----- Install Packages (TRIHD) -----
+
+        private const string GIT_GIST_TRIHD_USERNAME = "ht0x";
+	    private const string GIT_GIST_TRIHD_2D_URP_PACKAGE_ID = "11c9dcb1c1d777524df86960234fd6ae";
+        
+        [MenuItem("Tools/TriHD/Load New Project Manifest/2D Urp")]
+        static async Task Load_2D_Urp_Manifest()
+        {
+            EditorUtility.DisplayProgressBar("Load project manifest", "Loading", 1f);
+            var result = await PackageInstallation.LoadProjectManifestFromGist(GIT_GIST_TRIHD_USERNAME, GIT_GIST_TRIHD_2D_URP_PACKAGE_ID);
+            EditorUtility.ClearProgressBar();
+
+            var msg = result ? "Done!" : "Can't load 2d urp project manifest. Please see console.";
+            EditorUtility.DisplayDialog("Load project manifest", msg, "Ok");
+        }
+        
+        #endregion
+        
+        #region ----- Install Package (Unity) -----
+        
+        // [MenuItem("Tools/TriHD/Install Package/DoTween")]
+        // static void InstallDoTween()
+        // {
+        //     
+        // }
+        //
+        // [MenuItem("Tools/TriHD/Install Package/UniTask")]
+        // static void InstallUniTask()
+        // {
+        //     
+        // }
+        
+        [MenuItem("Tools/TriHD/Install Package/Unity New Input System")]
+        static async Task InstallUnityInputSystem()
+        {
+            EditorUtility.DisplayProgressBar("Install unity new input system", "Installing", 1f);
+            var result = await PackageInstallation.InstallPackage("inputsystem");
+            EditorUtility.ClearProgressBar();
+            
+            var msg = result ? "Done!" : "Can't install unity new input system. Please see console.";
+            EditorUtility.DisplayDialog("Install unity new input system", msg, "Ok");
+        }
+        
+        #endregion
     }
 }
--- InstallPackages/Editor/PackageInstallation.cs
+++ InstallPackages/Editor/PackageInstallation.cs
@@ -1,60 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
 
-public class PackageInstallation : MonoBehaviour
+public static class PackageInstallation
 {
 	#region ----- Variables ----
-        
-    #endregion
-        
-    #region ----- Properties -----
-        
-    #endregion
-    
-    #region ----- Unity Methods -----
-    
-    private void Awake()
-    {
-        
-    }
 
-    private void OnEnable()
-    {
-                    
-    }
-
-    private void Start()
-    {
-                
-    }
-        
-    private void Update()
-    {
-                
-    }
-
-    private void OnDisable()
-    {
-                    
-    }
-
-    private void OnDestroy()
-    {
-                    
-    }
-    
+	private const string GIT_GIST_FRONT_END_PROTOCOL = "https";
+	private const string GIT_GIST_BASE_ADDRESS = "gist.github.com";
+	private static readonly string PROJECT_MANIFEST_JSON_PATH = Path.Combine(Application.dataPath, "../Packages/manifest.json");
+	
     #endregion
     
     #region ----- Main Methods -----
-        
+
+    public static async Task<bool> InstallPackage(string packageName)
+    {
+	    var addRequest = Client.Add($"com.unity.{packageName}");
+	    while (!addRequest.IsCompleted || addRequest.Status == StatusCode.InProgress)
+		    await Task.Yield();
+
+	    var result = addRequest.Status == StatusCode.Success;
+	    if (!result) 
+		    Debug.LogError($"[PackageInstallation-InstallPackage] Unable to install package name: {packageName}." +
+		                   $"\nStatus code: {addRequest.Status}" +
+		                   $"\nError code: {addRequest.Error.errorCode}" +
+		                   $"\nError msg: {addRequest.Error.message}");
+	    
+	    return result;
+    }
+    
+    public static async Task<bool> LoadProjectManifestFromGist(string gistUserName, string gistId)
+    {
+	    var gistUrl = GetGistUrl(gistUserName, gistId);
+	    var packageJsonContent = await GetPackageJsonContent(gistUrl);
+	    if (string.IsNullOrEmpty(packageJsonContent)) 
+		    return false;
+	    
+	    ReplacePackageFile(PROJECT_MANIFEST_JSON_PATH, packageJsonContent);
+	    return true;
+    }
+
+    static void ReplacePackageFile(string path, string newContent)
+    {
+	    File.WriteAllText(path, newContent);
+	    Client.Resolve();
+    }
+    
     #endregion
-        
-    #region ----- Event Methods -----
-        
-    #endregion
-        
+    
     #region ----- Utility Methods -----
-        
+
+    static async Task<string> GetPackageJsonContent(string url)
+    {
+	    using var httpRequest = new HttpClient();
+	    var response = await httpRequest.GetAsync(url);
+	    if (!response.IsSuccessStatusCode)
+	    {
+		    Debug.LogError($"[PackageInstallation-GetPackageJsonContent] Failed to get response.\nUrl: {url}\nStatus Code: {(int)response.StatusCode} - {response.StatusCode.ToString()}");
+		    return string.Empty;
+	    }
+
+	    var content = await response.Content.ReadAsStringAsync();
+	    return content;
+    }
+    
+    static string GetGistUrl(string user, string id)
+    {
+	    return GIT_GIST_FRONT_END_PROTOCOL + "://" + GIT_GIST_BASE_ADDRESS + "/" + user + "/" + id;
+    }
+    
     #endregion
 }
